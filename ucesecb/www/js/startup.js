a4806cacf7ade6bca705a608a24825ff0a9f7a31
Elliot Anthony <ucesecb@ucl.ac.uk>
2019-03-07 15:16:38 -0500

    Adding the form functionality

--- ucesecb/www/js/startup.js
+++ ucesecb/www/js/startup.js
@@ -2,10 +2,15 @@ function trackAndCircle(){
 	trackLocation()
 	addPointLinePoly()
 	//getEarthquakes()
+	loadW3HTML()
 }
 
 function startup() {
 document.addEventListener('DOMContentLoaded', function() {
 trackAndCircle ();
 }, false);
+}
+
+function loadW3HTML() {
+	w3.includeHTML();
 }
